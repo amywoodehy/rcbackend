022d8a39d5ac7335a5822fb5f5d5948cd2192e66
Amy Woodehy <amywoodehy@gmail.com>
2016-04-08 09:35:11 +0600

    api auth url for rest framework

--- rcbackend/rcbackend/urls.py
+++ rcbackend/rcbackend/urls.py
@@ -17,7 +17,6 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', admin.site.urls),
     url(r'^cache/', include('mangacache.urls'))
 ]
